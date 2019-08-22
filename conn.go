56e546da5b7af9ce97b631fdcfdf146a662d09f0
Charling <56430114@qq.com>
2019-08-22 14:25:51 +0800

    add conn/pool

---
+++ conn.go
@@ -0,0 +1,118 @@
+package mysql
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Conn struct {
+	DriverName string
+	DataSourceName string
+	MaxOpenConns int 		//用于设置最大打开的连接数，默认值为0表示不限制		
+	MaxIdleConns int		//用于设置闲置的连接数
+	SQLDB *sql.DB
+}
+
+func connectMySQL(host, database, user, password, charset string, maxOpenConns, maxIdleConns int) *Conn {
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=%s&autocommit=true", user, password, host, database, charset)
+	db := &Conn {
+		DriverName: "mysql",
+		DataSourceName: dataSourceName,
+		MaxOpenConns: maxOpenConns,
+		MaxIdleConns: maxIdleConns,
+	}
+	var err error
+	db.SQLDB, err = sql.Open(db.DriverName, db.DataSourceName)
+	if err != nil {
+		log.Println("open mysql failed err=", err)
+		return nil
+	}
+	if err = db.SQLDB.Ping(); err != nil {
+		log.Println("ping mysql failed err=", err)
+		return nil
+	}
+	db.SQLDB.SetMaxOpenConns(db.MaxOpenConns)
+	db.SQLDB.SetMaxIdleConns(db.MaxIdleConns)
+	return db
+}
+
+func (db *Conn) close() error {
+	return db.SQLDB.Close()
+}
+
+func (db *Conn) ping() error {
+	return db.SQLDB.Ping()
+}
+
+func (db *Conn) execute(sqlStr string, args ...interface{}) (sql.Result, error) {
+	return db.SQLDB.Exec(sqlStr, args...)
+}
+
+func (db *Conn) GetDB() *sql.DB {
+	return db.SQLDB
+}
+
+func (db *Conn) Update(sqlStr string, args ...interface{}) (int64, error) {
+	res, err := db.execute(sqlStr, args...)
+	if err != nil {
+		return 0, err
+	}
+	
+	affect, err := res.RowsAffected()
+	return affect, err
+}
+
+func (db *Conn) Insert(sqlStr string, args ...interface{}) (int64, error) {
+	result, err := db.execute(sqlStr, args...)
+	if err != nil {
+		return 0, err
+	}
+
+	lastid, err := result.LastInsertId()
+	return lastid, err
+}
+
+func (db *Conn) Delete(sqlStr string, args ...interface{}) (int64, error) {
+	result, err := db.execute(sqlStr, args...)
+	if err != nil {
+		return 0, err
+	}
+
+	affect, err := result.RowsAffected()
+	return affect, err
+}
+
+func (db *Conn) Query(sqlStr string, args ...interface{}) ([]map[string]interface{}, error) {
+	rows, err := db.SQLDB.Query(sqlStr, args...)
+	if err != nil {
+		log.Println(err)
+		return []map[string]interface{}{}, err
+	}
+
+	defer rows.Close()
+    columns, _ := rows.Columns()
+	scanArgs := make([]interface{}, len(columns))
+	values := make([]interface{}, len(columns))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+    rowsMap := make([]map[string]interface{}, 0, 10)
+	for rows.Next() {
+		err = rows.Scan(scanArgs...)
+		rowMap := make(map[string]interface{})
+		for i, col := range values {
+			if col != nil {
+				rowMap[columns[i]] = string(col.([]byte))
+			}
+		}
+		rowsMap = append(rowsMap, rowMap)
+	}
+	
+    if err = rows.Err(); err != nil {
+		return []map[string]interface{}{}, err
+	}
+
+	return rowsMap, nil
+}
---
+++ pool.go
@@ -0,0 +1,63 @@
+package mysql
+
+import (
+	"log"
+	"yn.com/ext/common/gomsg"
+	"time"
+)
+
+var (
+	ins *Conn
+	host string
+	database string
+	user string
+	password string
+)
+
+func Starup(h, d, u, p string) *Conn {
+	host = h
+	database = d
+	user = u
+	password = p
+
+	ins = connectMySQL(host, database, user, password, "utf8", 2000, 1000)
+	if ins == nil {
+		log.Println("connect mysql failed ...")
+		return nil
+	}
+
+	go ins.polling()
+
+	return ins
+}
+
+func Stack() {
+	gomsg.Recover()
+
+	ins.reconnect()
+	go ins.polling()
+}
+
+func (s *Conn) reconnect() {
+	err := ins.close()
+	if err != nil {
+		log.Println("close mysql failed ...")
+		return
+	}
+	ins = connectMySQL(host, database, user, password, "utf8", 2000, 1000)
+}
+
+func (s *Conn) polling() {
+	defer Stack()
+
+	timer := time.NewTicker(300 * time.Second)
+	for {
+		select {
+		case <-timer.C:
+			err := ins.ping()
+			if err != nil {
+				ins.reconnect()
+			} 
+		}
+	}
+}
